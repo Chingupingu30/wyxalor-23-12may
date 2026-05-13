440a15ef2086c4bbb29e4955251a240b1c1c3728
Chingupingu30 <teodavid2994@gmail.com>
2026-05-13 23:33:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 <html>
 
 <head>
+   <!-- Privacy-friendly analytics by Plausible -->
+<script async src="https://plausible.io/js/pa-F6d7a890nTnQnfA_i5GJL.js"></script>
+<script>
+  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()
+</script>
+
    <!-- Histats.com  START  (aync)-->
 <script type="text/javascript">var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,5026082,4,0,0,0,00010000']);
